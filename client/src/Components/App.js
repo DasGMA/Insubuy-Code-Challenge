3cabca02c3d5218fb7720593b194b9548d6eb66b
dasgma <dasgrigoma@gmail.com>
2019-04-26 18:35:29 -0700

    Getting quotes on the results page

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import '../App.css';
 import QuoteForm from './QuoteForm/QuoteForm';
+import ResultsPage from './ResultsPage/ResultsPage';
 
 function App() {
   return (
     <div className="App">
-     <QuoteForm />
+      <Switch>
+        <Route exact path = '/' component = { QuoteForm } />
+        <Route path = '/results' component = { ResultsPage } />
+      </Switch>
     </div>
   );
 }
--- client/src/Components/ResultsPage/ResultsPage.js
+++ client/src/Components/ResultsPage/ResultsPage.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+
+const url = 'http://localhost:8080/quotes/';
 
 class ResultsPage extends Component {
     constructor(props){
         super(props)
-            state = {
-
+            this.state = {
+                quotes: []
             }
+    }
+
+    componentDidMount() {
+        Axios.get(url)
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    quotes: res.data.quotes
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     render() {
         return (
-            <div></div>
+            <div>{this.state.quotes.map(quote => {
+                return(
+                    <div>
+                        <div>{quote.name}</div>
+                        <div>{quote.description}</div>
+                        <div>{quote.price}</div>
+                        <div>{quote.type}</div>
+                        <div>{quote.section}</div>
+                    </div>
+                )
+            })}</div>
         )
     }
 }
